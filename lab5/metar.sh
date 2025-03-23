39572ddbced932f441bf4b4c3d584f0c304557fb
Shreyas Agnihotri <shreyas.agnihotri.2005@gmail.com>
2025-03-23 02:20:33 -0400

    Update metar.sh

--- lab5/metar.sh
+++ lab5/metar.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-
-echo "gathering data"
+echo "Gathering data" 
 curl -sL "https://aviationweather.gov/api/data/metar?ids=KMCI&format=json&taf=f$
-
 times=$(jq -r '.[].receiptTime' data.json | head -n 6)
 echo "$times"
-
 temps=$(jq '.[].temp' data.json)
 sum=0
-#!/bin/bash
+count=0
+for temp in $temps; do
+  sum=$(echo "$sum + $temp" | bc)
+  count=$((count + 1))
+done
+avg_temp=$(echo "scale=2; $sum / $count" | bc)
+echo "Average Temperature: $avg_temp"
+clouds=$(jq -r '.[].clouds' data.json | grep -v "CLR" | wc -l)
+cloudy=false
+if [ "$clouds" -gt 6 ]; then
+  cloudy=true
+fi
+echo "Mostly Cloudy: $cloudy"
 
-echo "gathering data"
-curl -sL "https://aviationweather.gov/api/data/metar?ids=KMCI&format=json&taf=f$
-
-times=$(jq -r '.[].receiptTime' data.json | head -n 6)
-echo "$times"
-
-temps=$(jq '.[].temp' data.json)
