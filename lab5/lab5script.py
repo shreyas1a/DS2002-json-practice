7eedc88a9000907013f713244338e7e01b255e0f
Shreyas Agnihotri <shreyas.agnihotri.2005@gmail.com>
2025-03-23 02:13:02 -0400

    Update lab5script.py

--- lab5/lab5script.py
+++ lab5/lab5script.py
@@ -1,12 +1,12 @@
 import json
+with open('../data/schacon.repos.json', 'r') as inputf:
+    info = json.load(inputf)
+with open('chacon.csv', 'w') as outputf:
+    for repos in info[:5]:  
+        repo_name = repos.get("name")
+        repo_url = repos.get("html_url")
+        updated_at = repos.get("updated_at")
+        visibility = repos.get("visibility", "public")
+        outputf.write(f"{repo_name},{repo_url},{updated_at},{visibility}\n")
 
-with open('chacon.csv', 'w') as output_file:
-    for repo_info in repositories[:5]:  
-        repo_name = repo_info.get("name")
-        repo_url = repo_info.get("html_url")
-        last_updated = repo_info.get("updated_at")
-        repo_visibility = repo_info.get("visibility", "public")
-        output_file.write(f"{repo_name},{repo_url},{last_updated},{repo_visibility}\n")
 
-with open('../data/schacon.repos.json', 'r') as input_file:
-    repositories = json.load(input_file)
